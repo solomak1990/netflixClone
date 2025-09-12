456d30f93f0dbaae250eafb6ade69030c47cbe79
Solomon Hunegnaw <hunegnaws@gmail.com>
2025-09-12 21:33:21 +0300

    first commit

---
+++ netflixClone-2025/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./Page/Home/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
